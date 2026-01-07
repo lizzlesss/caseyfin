bff04383ff48aa9eb817a94e1aaaee695d4579f4
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2026-01-07 10:29:25 -0500

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,10 +5,6 @@ set -ouex pipefail
 # enable rpmfusion
 dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-# intel media driver and codecs
-dnf -y install \
-    mpv
-
 # packages
 dnf -y install \
     android-tools \
@@ -21,7 +17,7 @@ dnf -y install \
 dnf5 install -y https://github.com/nbfc-linux/nbfc-linux/releases/download/0.3.19/fedora-nbfc-linux-0.3.19-1.x86_64.rpm
 
 # legcord
-dnf5 install -y https://github.com/Legcord/Legcord/releases/download/v1.1.6/Legcord-1.1.6-linux-x86_64.rpm
+dnf -y install https://github.com/Legcord/Legcord/releases/download/v1.1.6/Legcord-1.1.6-linux-x86_64.rpm
 
 # copr repos
 dnf -y copr enable bieszczaders/kernel-cachyos-addons
